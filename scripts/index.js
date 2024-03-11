416e7060fb5606c1da5bcb6e501c785dce416ec7
Victor Amesti <vamestif@gmail.com>
2024-03-11 14:53:39 -0400

    Correcion de Homework2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,55 @@
 import { technologys } from "./tech.js";
 
+class Activity {
+  constructor(id, title, description, imgUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.imgUrl = imgUrl;
+  }
+}
+
+class Repository {
+  constructor() {
+    this.activities = [];
+  }
+
+  saveActivity(param) {
+    this.activities.push(param);
+  }
+
+  /**Solicita un elemento para construir la etiqueta */
+  newActivity(element) {
+    const getId = document.getElementById(element);
+
+    while (getId.firstChild) {
+      getId.removeChild(getId.firstChild);
+    }
+
+    this.activities.forEach((activities) => {
+      const setCard = document.createElement("article");
+      const frame = document.createElement("div");
+
+      setCard.classList.add("card");
+      frame.appendChild(document.createElement("img")).src = activities.imgUrl;
+      //form.img;
+      setCard.appendChild(frame);
+
+      const title = setCard.appendChild(document.createElement("p")); //form.title;
+      const description = setCard.appendChild(document.createElement("p")); //form.descriptcion;
+      setCard.id = activities.id;
+
+      title.classList.add("title-card");
+      title.textContent = activities.title;
+      description.classList.add("body-card");
+      description.textContent = activities.description;
+
+      /** Para vaciar el formulario */
+      getId.append(setCard);
+    });
+  }
+}
+
 window.onload = () => {
   for (const key in technologys) {
     let box = document.getElementById("favTec");
@@ -19,6 +69,8 @@ window.onload = () => {
   submitButton.addEventListener("submit", getData);
 };
 
+const repo = new Repository();
+
 function getData(event) {
   /* Detiene el evento del submit para evitar que recarge la pagina */
   event.preventDefault();
@@ -27,28 +79,17 @@ function getData(event) {
 
   const form = {
     title: getForm.get("title"),
+    description: getForm.get("description"),
     img: getForm.get("image"),
-    descriptcion: getForm.get("description"),
   };
 
-  const getId = document.getElementById("addActivity");
-  const setCard = document.createElement("article");
-  const frame = document.createElement("div");
+  /**Genera el ID contando los elementos en el div de actividades y luego lo asigna la nueva actividad */
+  const id = document.querySelectorAll("#addActivity").length;
+  const newActivity = new Activity(id, form.title, form.description, form.img);
 
-  setCard.classList.add("card");
-  frame.appendChild(document.createElement("img")).src = form.img;
-  //form.img;
-  setCard.appendChild(frame);
-
-  const title = setCard.appendChild(document.createElement("p")); //form.title;
-  const description = setCard.appendChild(document.createElement("p")); //form.descriptcion;
-
-  title.classList.add("title-card");
-  title.textContent = form.title;
-  description.classList.add("body-card");
-  description.textContent = form.descriptcion;
-
-  /** Para vaciar el formulario */
+  /**Se aguarda la actividad para aplicar la logica  */
+  repo.saveActivity(newActivity);
+  /*Crea el componente */
+  repo.newActivity("addActivity");
   event.target.reset();
-  getId.append(setCard);
 }
